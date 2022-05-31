1510b1b2336dc059d939ccf236b6dcb789f7de01
thanhngan22 <nganbui9a2017@gmail.com>
2022-05-31 22:20:58 +0700

    day 14 : error handling

--- day14/main.js
+++ day14/main.js
@@ -1,1 +1,44 @@
-// Day 08: 27/05/2022 : Objects
+// Day 14: error handling
+
+try {
+    let lastName = 'Yetayeh'
+    let fullName = fistName + ' ' + lastName
+  } catch (err) {
+    console.log(err)
+  }
+
+  try {
+    let lastName = 'Yetayeh'
+    let fullName = fistName + ' ' + lastName
+  } catch (err) {
+    console.error(err) // we can use console.log() or console.error()
+  } finally {
+    console.log('In any case I will be executed')
+  }
+
+  try {
+    let lastName = 'Yetayeh'
+    let fullName = fistName + ' ' + lastName
+  } catch (err) {
+    console.log('Name of the error', err.name)
+    console.log('Error message', err.message)
+  } finally {
+    console.log('In any case I will be executed')
+  }
+
+  const throwErrorExampleFun = () => {
+    let message
+    let x = prompt('Enter a number: ')
+    try {
+      if (x == '') throw 'empty'
+      if (isNaN(x)) throw 'not a number'
+      x = Number(x)
+      if (x < 5) throw 'too low'
+      if (x > 10) throw 'too high'
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  throwErrorExampleFun()
+
+  
